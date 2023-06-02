976504699a2260f23ca35892b288b3d654c22b36
Anna7682 <107514746+Anna7682@users.noreply.github.com>
2023-06-02 17:54:05 +0300

    Create app.js

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+/* eslint-disable import/extensions */
+import Widget from './Widget';
+
+const widget = new Widget('https://ahj-ws-backend-6pyi.onrender.com');
+
+// const widget = new Widget('ws://localhost:8080/ws');
+
+widget.createForm();
