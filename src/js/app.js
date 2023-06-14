32713dac90693bf4345c5faf1bde511e755fa507
Anna7682 <107514746+Anna7682@users.noreply.github.com>
2023-06-14 20:05:01 +0300

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/extensions */
 import Widget from './Widget';
 
-const widget = new Widget('wss://ahj-sse-ws-chat.herokuapp.com/ws');
+const widget = new Widget('https://ahj-ws-backend-6pyi.onrender.com/');
 
 // const widget = new Widget('ws://localhost:8080/ws');
 
